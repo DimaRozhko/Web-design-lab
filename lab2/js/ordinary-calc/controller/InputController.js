2eb2a0f625745de846bb1ea375cf9ec149979e45
dima <dimarozhko2000@gmail.com>
2021-03-26 10:47:55 +0200

    ordinary-calc

--- lab2/js/ordinary-calc/controller/InputController.js
+++ lab2/js/ordinary-calc/controller/InputController.js
@@ -17,10 +17,25 @@ export default class InputController {
             this.calculate.calculate(expression.value, key);
             expression.value = '';
         }
+        if (key === '!') {
+            this.calculate.calculate(this.calculate.factorial(expression.value), lastValue);
+            expression.value = '';
+        }
+        if (key === ')') {
+            this.calculate.calculate(this.calculate.parseFunc(expression.value), lastValue);
+            expression.value = '';
+        }
         if (key === 'Enter') {
             this.calculate.calculate(expression.value, lastValue);
             expression.value = '';
         }
         return sign;
+    }
+
+    validInputKeyup(key, expression) {
+        if (key === '+' | key === '-' | key === '/'  | key === '*' | key === '%' 
+            | key === '!' | key === ')' | key === 'Enter') {
+            expression.value = '';
+        }
     }
 }
--- lab2/js/ordinary-calc/model/Calculate.js
+++ lab2/js/ordinary-calc/model/Calculate.js
@@ -1,9 +1,20 @@
+
 export default class Calculate {
     constructor () {
         this.result = 0;
     }
 
     getResult() { return this.result; }
+
+    factorial(expression) {
+        expression = parseFloat(expression.replace('+', '').replace('-', '')
+            .replace('*', '').replace('/', '').replace('%', ''));
+        let res = 1;
+        for (let i = 2; i <= expression; i++) {
+            res *= i;
+        }
+        return res.toString();
+    }
 
     calculate (expression, operation) {
         if (expression.match(/[0-9]/)) {
@@ -23,8 +34,32 @@ export default class Calculate {
                 case '%':
                     this.result *= parseFloat(expression.replace('%', '')) / 100;
                     break;
+                default:
+                    this.result = parseFloat(expression);
+                    break;
             }
         }
         return this.result;
+    }
+
+    parseFunc(expression) {
+        let res = 1;
+        expression = expression.replace(' ', '');
+        if (expression.search(/log/i) != -1) {
+            res = Math.log10(parseFloat(expression.replace('log(', '')));
+        } else if (expression.search(/ln/i) != -1) {
+            res = Math.log(parseFloat(expression.replace('ln(', '')));
+        } else if (expression.search(/cos/i) != -1) {
+            res = Math.cos(parseFloat(expression.replace('cos(', '')));
+        } else if (expression.search(/sin/i) != -1) {
+            res = Math.sin(parseFloat(expression.replace('sin(', '')));
+        } else if (expression.search(/^/i) != -1) {
+            res = expression.substring(0, expression.indexOf('^'));
+            res = Math.pow(res, expression.substring(expression.indexOf('^') + 2, expression.length));
+        }
+        if (expression.search(/sqrt/i) != -1) {
+            res = Math.sqrt(parseFloat(expression.replace('sqrt(', '')));
+        }
+        return res.toString();
     }
 }
